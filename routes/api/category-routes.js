1440aa0763f5176dbf04cbb55602fe39ad7c209a
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-08-01 16:40:59 -0500

    corrected jsom to json to fix my errors

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,15 +7,14 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    attributes: ['id', 'category_name'],
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        attributes: ['product_name', 'price', 'stock'],
       },
     ],
   })
-    .then((dbCategoryData) => res.jsom(dbCategoryData))
+    .then((dbCategoryData) => res.json(dbCategoryData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
